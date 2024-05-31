a6c4a5d4001975910dd1942d351d8f6fe7593773
vera-alex <alexjorgeabelverapinto@gmail.com>
2024-05-31 13:57:18 -0400

    modificación en el app.module.ts

--- be_p2_ajavp/src/app.module.ts
+++ be_p2_ajavp/src/app.module.ts
@@ -2,9 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SeriesModule } from './series/series.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [SeriesModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as any,
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '**/*.entity.ts'],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+    SeriesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
