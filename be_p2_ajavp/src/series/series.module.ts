47c60a8c02964db94d63da6982171f8d364bed12
vera-alex <alexjorgeabelverapinto@gmail.com>
2024-05-31 19:14:21 -0400

    modificación del archivo series.module.ts

--- be_p2_ajavp/src/series/series.module.ts
+++ be_p2_ajavp/src/series/series.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SeriesService } from './series.service';
 import { SeriesController } from './series.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Serie } from './entities/serie.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Serie])],
   controllers: [SeriesController],
   providers: [SeriesService],
 })
