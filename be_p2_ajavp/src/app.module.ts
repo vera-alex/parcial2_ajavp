cdbb289eede8d649da3f62d3ae06ee67bc246f9b
vera-alex <alexjorgeabelverapinto@gmail.com>
2024-05-31 13:38:57 -0400

    creación de la entidad series

--- be_p2_ajavp/src/app.module.ts
+++ be_p2_ajavp/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SeriesModule } from './series/series.module';
 
 @Module({
-  imports: [],
+  imports: [SeriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
