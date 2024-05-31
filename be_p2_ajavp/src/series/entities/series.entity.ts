284ca507fdef222a4846cf52fb56586b6b0852a0
vera-alex <alexjorgeabelverapinto@gmail.com>
2024-05-31 14:43:57 -0400

    cambio en el archivo series.entity.ts

--- be_p2_ajavp/src/series/entities/series.entity.ts
+++ be_p2_ajavp/src/series/entities/series.entity.ts
@@ -5,18 +5,18 @@ export class Series {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column('varchar', { length: 250 })
+  @Column('varchar', { length: 250, nullable: false })
   titulo: string;
 
-  @Column('varchar', { length: 5000 })
+  @Column('varchar', { length: 5000, nullable: false })
   sinopsis: string;
 
-  @Column('varchar', { length: 100 })
+  @Column('varchar', { length: 100, nullable: false })
   director: string;
 
-  @Column('int')
+  @Column('int', { nullable: false })
   temporadas: number;
 
-  @CreateDateColumn({ name: 'fecha_estreno' })
+  @CreateDateColumn({ name: 'fecha_estreno', nullable: false })
   fechaEstreno: Date;
 }
