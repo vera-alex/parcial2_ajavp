687d94486120f2a0653b45c24b5003ddfff09b0d
vera-alex <alexjorgeabelverapinto@gmail.com>
2024-05-31 14:23:08 -0400

    modificación en el archivo series.entity.ts

--- be_p2_ajavp/src/series/entities/series.entity.ts
+++ be_p2_ajavp/src/series/entities/series.entity.ts
@@ -1,1 +1,22 @@
-export class Series {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('series')
+export class Series {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('varchar', { length: 250 })
+  titulo: string;
+
+  @Column('varchar', { length: 5000 })
+  sinopsis: string;
+
+  @Column('varchar', { length: 100 })
+  director: string;
+
+  @Column('int')
+  temporadas: number;
+
+  @CreateDateColumn({ name: 'fecha_estreno' })
+  fechaEstreno: Date;
+}
